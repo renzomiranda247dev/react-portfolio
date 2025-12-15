5594b6d79fc815ea99e9c18acdcdf8d6142035b2
renzoMirandaDev <nalamypuppy02@gmail.com>
2025-12-15 19:14:21 +0800

    im the lebron james of physical therapy

--- src/content_option.js
+++ src/content_option.js
@@ -1,11 +1,11 @@
-const logotext = "JOHN";
+const logotext = "RENZO";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Renzo Miranda",
+    description: "I’m Renzo Miranda data scientist _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Renzo Miranda",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
